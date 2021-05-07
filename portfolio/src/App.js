7048a67bf7202ecff8c4537577244e3be5bf5e36
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-07 15:25:17 +0100

    structure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
+import  Header  from './components/Header'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
+    
     <div className="App">
-     
+    <Navigation />
+    <Header />
     </div>
   );
 }
---
+++ portfolio/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <ul>
+                <li>text</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation
