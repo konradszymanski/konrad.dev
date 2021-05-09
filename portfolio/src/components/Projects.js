95f539b57eb00726cd080edc30c0f419a1d642a5
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-09 14:49:41 +0100

    Create Projects.js

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>PROJECTS COMPONENT</h1>
+            <ul>
+                <li>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Projects
