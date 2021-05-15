1fca8a104b243908e6b5e8962f5c5fdb046f4978
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-15 13:24:49 +0100

    structure

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/footer.css'
+const Footer = () => {
+    return (
+        <footer>
+            <p>footer example</p>
+        </footer>
+    )
+}
+
+export default Footer
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Projects = () => {
+    const projects = [{
+        tile: 'konrad.stie',
+        description: 'asdasdasdasd afaf'
+    },
+    {
+        tile: 'asdasdasd.stie',
+        description: 'asdasdasassasdasd afaf'
+    },
+    {
+        tile: 'asdassasdasd.stie',
+        description: 'asdasdasdfghjasassasdasd afaf'
+    }]
+
+    const [project, setProject] = useState(0);
+
     return (
         <div>
             <h1>PROJECTS COMPONENT</h1>
             <ul>
-                  {/* 
-                each project need to have:
-                hyperlink
-                image
-                description
-                technologies used
-                */}
-                <li>Konrad.site</li>
-                <li>OrangeBlog</li>
-                <li>Whack-a-mole</li>
-                <li>jadwigaszymanska.com</li>
-                <li>Estate Agency</li>
-                <li>fruitmashine</li>
+                {projects.map((item, index) =>
+                 <li key={index} onClick={() =>  setProject(item)} >{item.tile}</li>)}
             </ul>
+
+            {project ? (
+                <div id='description'>
+                    {project.description}
+                </div>
+            ) : (
+                ""
+            )}
+
         </div>
+
     )
 }
 
