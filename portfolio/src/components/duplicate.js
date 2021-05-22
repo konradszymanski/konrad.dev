1830266de15ee3c3ccf61a7660b54207141fc8be
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-22 18:42:02 +0100

    scroll down on window height

--- portfolio/src/components/duplicate.js
+++ portfolio/src/components/duplicate.js
@@ -3,23 +3,16 @@ import '../styles/features.css'
 import logo from '../icons/arrowDown.png'
 // this component is edited is features.css file
 
-import { animateScroll as scroll } from 'react-scroll';
-
 const NextSection = () => {
-    
-    let windowH = window.innerHeight
-    
-    function scrollMoreDown() {
-        scroll.scrollMore(windowH);
-      }
+   
     return (
         <div>
         <button
             id='nextSetion'
             className='nextSection'
-            onClick={scrollMoreDown}
         >
             <img src={logo} alt='icon' />
+
         </button>
         
         </div>
