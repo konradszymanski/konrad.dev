2a27e096cb1c8002695495ba0dc1b51770a329fd
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-23 19:08:44 +0100

    updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,22 @@
 
-import  Header  from './components/Header'
+import Header from './components/Header'
 import MyStory from './components/MyStory'
 //import Navigation from './components/Navigation'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 
+
 function App() {
+
+ 
   return (
     //classNameApp edited in index.css
-    <div className="App" > 
-    <Header />
-    <MyStory />
-    <Projects />
-    <Footer />
+    <div className="App" >
+      <Header />
+   
+      <MyStory/>
+      <Projects />
+      <Footer />
     </div>
   );
 }
--- portfolio/src/components/MyStory.js
+++ portfolio/src/components/MyStory.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,  { useRef } from 'react'
 import '../styles/myStory.css'
 import NextSection from './NextSection'
 
 const MyStory = () => {
+
+    const mystory = useRef(null);
+
     return (
-        <section className='bio' id='biography'>
+        <section 
+        ref={mystory}
+        className='bio'
+         id='biography'>
             <h2>Bio</h2>
             <p>blabla bla bla bla bla bla  </p>
             <NextSection />
