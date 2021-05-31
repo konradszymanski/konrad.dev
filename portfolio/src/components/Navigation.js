a1211e12c355a7ad45df0a1147989188b682b3fc
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-05-31 13:09:47 +0100

    scroll menu items

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import '../styles/navigation.css'
-import { NavLink as Link } from 'react-router-dom'
+import { Link } from 'react-scroll'
 
 const Navigation = () => {
 
+    // <Link 
+    // activeClass="active" 
+    // to="test1" 
+    // spy={true} 
+    // smooth={true} 
+    // offset={50} 
+    // duration={500} 
+    // onSetActive={this.handleSetActive}>
+    //       Test 1
+    // </Link>
     return (
 
         <nav>
             <ul>
                 <li>
-                    <Link to={'#biography'}>Bio</Link>
+                    <Link to={'biography'}>Bio</Link>
                 </li>
 
                 <li>
-                    <Link to={'#projects'}>Projects</Link>
+                    <Link to={'projects'}>Projects</Link>
                 </li>
                 <li>
-                    <Link to={'#footer'}>Footer</Link>
+                    <Link to={'footer'}>Contact</Link>
                 </li>
             </ul>
         </nav>
