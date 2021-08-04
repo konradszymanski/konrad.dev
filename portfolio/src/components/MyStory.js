c868b4fc4fb6caebb6225e74c94d33ed3a64d91a
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-08-04 20:13:36 +0100

    bio css

--- portfolio/src/components/MyStory.js
+++ portfolio/src/components/MyStory.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import '../styles/myStory.css'
 import Circle from './Circle';
-import NextSection from './NextSection'
+// import NextSection from './NextSection'
 
 const MyStory = () => {
 
@@ -15,22 +15,25 @@ const MyStory = () => {
                     
                     <p className='helloWorld'>Hello World!</p>
                     <p>
-                        <br />My name is <span>Konrad</span> Szymanski. Based in UK.
+                        <br />My name is <span>Konrad</span> Szymanski. 
                     </p>
                     <p>
-                      Self-motivated, determined front-end <span>web developer </span>  with a high level of experience working on multiple projects. Offering several years of expertise in fast-paced departments. I have a strong ability to work closely with people of all levels, different backgrounds and personalities. 
+                    I am a Self-motivated<span className='coma'>,</span> determined front-end <span>web developer </span>  with a high level of experience working on multiple projects. Offering several years of expertise in fast-paced departments<span className='coma'>,</span> with strong ability to work closely with people of all levels<span className='coma'>,</span> different backgrounds and personalities. 
                     </p>
                     <p> 
-                      Newly graduated web developer offering enthusiasm and understanding of various programming languages. 
+                      Newly graduated web developer offering <span>enthusiasm</span> and <span>understanding</span> of various programming languages. 
                     </p>
                     <p>
-                        Innovative<span className='coma'>,</span> proactive<span className='coma'>,</span> and capable of developing elegant and application designs for demanding clients. Talented in project management<span className='coma'>,</span> team leadership and independent problem-solving abilities. Highly organised<span className='coma'>,</span> proficient at multitasking and enhancing designs and verifying code.
+                        <span>Innovative<span className='coma'>,</span> proactive<span className='coma'></span>,</span> and capable of developing elegant application designs for demanding clients. 
                     </p>
                     <p>
-                        Engaged in sports<span className='coma'>,</span> absolutely delighted of David Fincher's movies<span className='coma'>,</span> fascinated by medieval castles
+                        Talented in project management<span className='coma'>,</span> team leadership and independent <span>problem-solving</span> abilities. Highly <span>organised</span><span className='coma'>,</span> proficient at multitasking and enhancing designs and verifying code.
                     </p>
                     <p>
-                        I am looking to join a company where the opportunity for growth and personal development is embraced.
+                        Engaged in sports<span className='coma'>,</span> absolutely delighted of David Fincher's movies<span className='coma'>,</span> fascinated by medieval castles.
+                    </p>
+                    <p>
+                        I am looking to join a company where the opportunity for <span>growth and personal development</span> is embraced.
                     </p>
                     <p>You can reach me on
                         <span>
@@ -44,9 +47,10 @@ const MyStory = () => {
                 </article>
                 <article className='rightArticle'>
                     <Circle />
+                   
                 </article>
             </div>
-            <NextSection />
+            {/* <NextSection /> */}
         </section>
     )
 }
