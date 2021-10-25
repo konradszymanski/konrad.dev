23af4ac007d67beb2ac03bc86a7f26a75e839845
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-10-25 18:26:49 +0100

    bug flip fix

--- portfolio/src/components/Cards.js
+++ portfolio/src/components/Cards.js
@@ -6,43 +6,49 @@ import '../styles/projects.css'
 
 
 const Cards = ({ project }) => {
-  const [isFlipped, setIsFlipped] = React.useState(false);
-  // console.log(project);
-  return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-      <div
-        onClick={() => setIsFlipped((prev) => !prev)}
-      >
-        <div className='test'>
-          <h3>{project.title}</h3>
-          <img className='thumbnail' src={project.img} alt="img" />
-          <p>{project.type}</p>
-        </div>
-      </div>
-      <div
-        onClick={() => setIsFlipped((prev) => !prev)}
-        className="CardBack"
-      >
-        <h3>{project.title}</h3>
-        <p>{project.description}</p>
-        <ul className='projectList'>
-          <li>{project.tech}</li>
-          <li>{project.tech2}</li>
-          <li>{project.tech3}</li>
-        </ul>
-        <a className='projectLink' href={project.link} target='_blank' rel="noreferrer">go to project</a>
-      </div>
-    </ReactCardFlip>
-  );
+	const [isFlipped, setIsFlipped] = React.useState(false);
+	// console.log(project);
+	return (
+		<ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+			<div id="FRONT-PARENT">
+			<div
+				onClick={() => setIsFlipped((prev) => !prev)}
+			>
+				<div className='test'>
+					<h3>{project.title}</h3>
+					<img className='thumbnail' src={project.img} alt="img" />
+					<p>{project.type}</p>
+				</div>
+			</div>
+			</div>
+			<div id="BACK-PARENT">
+			<div
+				onClick={() => setIsFlipped((prev) => !prev)}
+				className="CardBack"
+			>
+				<h3>{project.title}</h3>
+				<p>{project.description}</p>
+				<ul className='projectList'>
+					<li>{project.tech}</li>
+					<li>{project.tech2}</li>
+					<li>{project.tech3}</li>
+				</ul>
+				{isFlipped && (
+				<a className='projectLink' href={project.link} target='_blank' rel="noreferrer">go to project</a>
+				)}	
+				</div>
+			</div>
+		</ReactCardFlip>
+	);
 };
 
 const Card = () => {
-  return (
-    <div className="Projects">
-      {Data.map((item, index) => (
-        <Cards project={item} key={`card-${index}`} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="Projects">
+			{Data.map((item, index) => (
+				<Cards project={item} key={`card-${index}`} />
+			))}
+		</div>
+	);
 };
 export default Card
