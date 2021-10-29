4e6c40500ba884e2a9132d0dcbc85313ce928733
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-10-29 20:38:14 +0100

    css update // shorten my bio

--- portfolio/src/components/MyStory.js
+++ portfolio/src/components/MyStory.js
@@ -18,16 +18,13 @@ const MyStory = () => {
                         <br />My name is <span>Konrad</span> Szymanski. 
                     </p>
                     <p>
-                    I am a Self-motivated<span className='coma'>,</span> determined front-end <span>web developer </span>  with a high level of experience working on multiple projects. Offering several years of expertise in fast-paced departments<span className='coma'>,</span> with strong ability to work closely with people of all levels<span className='coma'>,</span> different backgrounds and personalities. 
-                    </p>
-                    <p> 
-                      Newly graduated web developer offering <span>enthusiasm</span> and <span>understanding</span> of various programming languages. 
+                    I am a Self-motivated<span className='coma'>,</span> determined front-end <span>web developer</span><span className='coma'>,</span> with strong ability to work closely with people of all levels<span className='coma'>,</span> different backgrounds and personalities. 
                     </p>
                     <p>
                         <span>Innovative<span className='coma'>,</span> proactive<span className='coma'></span>,</span> and capable of developing elegant application designs for demanding clients. 
                     </p>
                     <p>
-                        Talented in project management<span className='coma'>,</span> team leadership and independent <span>problem-solving</span> abilities. Highly <span>organised</span><span className='coma'>,</span> proficient at multitasking and enhancing designs and verifying code.
+                        Experienced in team leadership and independent <span>problem-solving</span> abilities. Highly <span>organised</span><span className='coma'>,</span> proficient at multitasking and enhancing designs and verifying code.
                     </p>
                     <p>
                         Engaged in sports<span className='coma'>,</span> absolutely delighted of David Fincher's movies<span className='coma'>,</span> fascinated by medieval castles.
@@ -40,7 +37,7 @@ const MyStory = () => {
                             <a
                                 href='https://www.linkedin.com/in/konrad-szymanski-92790311a/'
                                 target='_blank'
-                                rel="noreferrer"> LinkedIn.
+                                rel="noreferrer"> LinkedIn<span className='coma'>!</span>
                             </a>
                         </span>
                     </p>
